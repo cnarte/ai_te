642739b7c7543a5159a0c3c503dabfa487dea807
cnarte <ankitkrchy.54@gmail.com>
2022-05-23 00:09:57 +0530

    c

---
+++ GroupB/assignment 3/MainObject.scala
@@ -0,0 +1,9 @@
+    import scala.collection.immutable._  
+    object MainObject{  
+        def main(args:Array[String]){  
+            var vector1 = Vector(5,8,3,6,9,4) //Or  
+            var vector2 = Vector(5,2,6,3)    
+            println(vector1)  
+            println(vector2)   
+        }  
+    }  
